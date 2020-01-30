3b71aba1222351f2622200458f8f230e0474ac7e
ASterniewski <drapiszon@gmailcom>
2020-01-30 16:52:06 +0100

    buttons modification

--- script.js
+++ script.js
@@ -58,13 +58,24 @@ let todoList = {
   }
 };
 
-let displayTodosButton = document.getElementById('displayTodosButton');
-let toggleAllButton = document.getElementById('toggleAllButton');
+// Old style buttons:
+// let displayTodosButton = document.getElementById('displayTodosButton');
+// console.log(displayTodosButton);
+// let toggleAllButton = document.getElementById('toggleAllButton');
 
-displayTodosButton.addEventListener('click', function() {
-  todoList.displayTodos();
-});
+// displayTodosButton.addEventListener('click', function() {
+//   todoList.displayTodos();
+// });
 
-toggleAllButton.addEventListener('click', function() {
-  todoList.toggleAll();
-});
+// toggleAllButton.addEventListener('click', function() {
+//   todoList.toggleAll();
+// });
+
+let handlers = {
+  displayTodos: function() {
+    todoList.displayTodos();
+  },
+  toggleAll: function() {
+    todoList.toggleAll();
+  }
+};
